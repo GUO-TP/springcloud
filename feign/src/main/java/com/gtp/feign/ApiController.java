46f029160a58d25d2356d770074da2c1c97d3faf
guotp <312841009@qq.com>
2021-12-03 14:38:30 +0800

    服务消费者:Feign 熔断器监控

---
+++ feign/src/main/java/com/gtp/feign/ApiController.java
@@ -0,0 +1,21 @@
+package com.gtp.feign;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+/*
+http://localhost:8081/hystrix
+http://localhost:8081/index
+*/
+
+@RestController
+public class ApiController {
+
+    @Autowired
+    private ApiService apiService;
+
+    @RequestMapping("index")
+    public String index(){
+        return apiService.index();
+    }
+}
--- feign/src/main/java/com/gtp/feign/ApiService.java
+++ feign/src/main/java/com/gtp/feign/ApiService.java
@@ -2,10 +2,12 @@ package com.gtp.feign;
 
 
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value = "eurekaclient")
+@Component
+@FeignClient(value = "eurekaclient",fallback = ApiServiceError.class)
 public interface ApiService {
 
     @RequestMapping(value = "/index", method = RequestMethod.GET)
