b49659350102280760f5b42dba7885a0f2f47091
guotp <312841009@qq.com>
2021-12-03 10:03:56 +0800

    服务注册与发现

---
+++ eurekaserver/src/test/java/com/gtp/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtp.eurekaserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
