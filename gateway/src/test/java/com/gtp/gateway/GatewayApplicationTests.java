ff2e73cd015f7d984f54e16918e287b612d65413
guotp <312841009@qq.com>
2021-12-03 10:38:47 +0800

    服务网关

---
+++ gateway/src/test/java/com/gtp/gateway/GatewayApplicationTests.java
@@ -0,0 +1,14 @@
+package com.gtp.gateway;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
